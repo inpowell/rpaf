d2e04f00513e1e1c726eeb4854f9243d21d0ee7a
Ian Powell <i.powell@unsw.edu.au>
2019-07-08 17:29:48 +1000

    Risk object documentation and binary GLM example

---
+++ R/risk.R
@@ -0,0 +1,60 @@
+#' Risk function information
+#'
+#' A standardised object containing information regarding risk functions needed
+#' to calculate PAFs.
+#'
+#' A risk object is a list that contains the following elements:
+#'
+#' \describe{
+#'
+#' \item{\code{riskfn(df, ...)}}{A function which takes a population as
+#' described in \code{df} and optional further arguments, and outputs the event
+#' risk for each member in the population.}
+#'
+#' \item{\code{dtransvar(df, ...)}}{A function that returns the derivatives of
+#' risks with respect to any underlying random parameters. The output should be
+#' a matrix with element in row \eqn{i} and column \eqn{j} \deqn{J_{i,j} =
+#' \frac{\partial r(x_i; \beta)}{\partial \beta_j}} where \eqn{x_i} corresponds
+#' to row \eqn{i} of the input dataframe.}
+#'
+#' \item{\code{vcov}}{The variance-covariance matrix of the estimated parameters
+#' \eqn{\beta_j}.}
+#'
+#' \item{\code{source_df}}{(optional) The source population used for risk
+#' function calculation.}
+#'
+#' }
+#'
+#' @name risk-object
+#' @family risk functions
+NULL
+
+#' Risk information for binary GLMs
+#'
+#' This function extracts information on the risk from generalised linear models
+#' with a binary predictor. This can be used for logistic regression, or other
+#' models with binary dependent variables, such as the probit model.
+#'
+#' @param model a fitted GLM
+#'
+#' @return a \code{\link{risk-object}} list
+#' @export
+#'
+#' @importFrom stats delete.response model.frame model.matrix predict.glm terms vcov
+#' @family risk functions
+risk_glm <- function(model) {
+  Terms <- delete.response(terms(model))
+  mu.eta <- model$family$mu.eta
+
+  list(
+    riskfn = function(df, ...) {
+      predict.glm(model, newdata = df, type = "response")
+    },
+    dtransvar = function(df, ...) {
+      X <- model.matrix(Terms, data = df, xlev = model$xlevels, contrasts.arg = model$contrasts)
+      X * mu.eta(drop(X %*% coef(model)))
+    },
+    vcov = vcov(model),
+    source_df = model.frame(model)
+  )
+}
